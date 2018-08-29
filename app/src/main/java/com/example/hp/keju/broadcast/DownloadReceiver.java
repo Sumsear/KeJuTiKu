6429a7f2e5f7094e330657aa793bd448e72a4648
xuzhenhao <309201338@qq.com>
2018-08-29 17:51:18 +0800

    修改应用更新功能

--- app/src/main/java/com/example/hp/keju/broadcast/DownloadReceiver.java
+++ app/src/main/java/com/example/hp/keju/broadcast/DownloadReceiver.java
@@ -4,9 +4,17 @@ import android.app.DownloadManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.support.v4.content.FileProvider;
 
+import com.example.hp.keju.BuildConfig;
+import com.example.hp.keju.R;
 import com.example.hp.keju.util.LogUtil;
+
+import java.io.File;
 
 public class DownloadReceiver extends BroadcastReceiver {
 
@@ -14,30 +22,59 @@ public class DownloadReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
 
         String action = intent.getAction();
+        long downloadId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
         LogUtil.e("action" + action);
         if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
-
             DownloadManager dm = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
-            try {
-                if (dm == null) return;
-                long id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
-                LogUtil.e("download id：" + id);
-                Uri uri = dm.getUriForDownloadedFile(id);
-                if (uri != null) {
-                    LogUtil.e("Uri = " + uri.toString());
-                    Intent install = new Intent(Intent.ACTION_VIEW);
-                    install.setDataAndType(uri, "application/vnd.android.package-archive");
-                    install.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    context.startActivity(install);
+            if (dm == null) return;
+            DownloadManager.Query query = new DownloadManager.Query();
+            query.setFilterById(downloadId);
+            Cursor cursor = dm.query(query);
+            if (cursor.moveToFirst()) {
+                int status = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS));
+                switch (status) {
+                    case DownloadManager.STATUS_SUCCESSFUL:
+                        install(context, dm, downloadId);
+                        break;
+                    case DownloadManager.STATUS_FAILED:
+                        LogUtil.e("下载失败!");
+                        break;
+                    case DownloadManager.STATUS_RUNNING:
+                        LogUtil.e("下载中!");
+                        break;
+                    case DownloadManager.STATUS_PAUSED:
+                        LogUtil.e("下载暂停!");
+                        break;
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
             }
+            cursor.close();
         } else if (DownloadManager.ACTION_NOTIFICATION_CLICKED.equals(action)) {
             //处理 如果还未完成下载，用户点击Notification ，跳转到下载中心
             Intent viewDownloadIntent = new Intent(DownloadManager.ACTION_VIEW_DOWNLOADS);
             viewDownloadIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(viewDownloadIntent);
+        }
+    }
+
+    /**
+     * TODO 安装APK
+     *
+     * @param context    上下文
+     * @param dm         DownloadManager
+     * @param downloadId download id
+     */
+    private void install(Context context, DownloadManager dm, long downloadId) {
+        try {
+            Intent install = new Intent(Intent.ACTION_VIEW);
+            Uri uri = dm.getUriForDownloadedFile(downloadId);
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+                install.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            }
+            install.setDataAndType(uri, "application/vnd.android.package-archive");
+            install.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(install);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
--- app/src/main/java/com/example/hp/keju/util/UpdateUtil.java
+++ app/src/main/java/com/example/hp/keju/util/UpdateUtil.java
@@ -4,13 +4,18 @@ import android.app.DownloadManager;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
 
 import com.example.hp.keju.R;
+
+import java.io.File;
 
 public class UpdateUtil {
 
     private static UpdateUtil instance;
     private static long downloadId = -1;
+    private Handler mHandler = new Handler(Looper.getMainLooper());
 
     private UpdateUtil() {
     }
@@ -33,18 +38,89 @@ public class UpdateUtil {
             LogUtil.e("DownloadManager != null");
             LogUtil.e("url: " + url);
             Uri uri = Uri.parse(url);
-            if (downloadId != -1){
+            if (downloadId != -1) {
                 dm.remove(downloadId);
             }
+            String title = context.getString(R.string.app_name);
+
+            File file = context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS + "/" + title);
+            if (file != null && file.exists()) {
+                file.delete();
+            }
             DownloadManager.Request request = new DownloadManager.Request(uri);
-            request.setTitle(context.getString(R.string.app_name));
+            request.setTitle(title);
             request.setDescription("一个正在慢慢完善的科举答题App");
-            request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI);
+            request.setAllowedOverMetered(true);
             request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
             request.setMimeType("application/vnd.android.package-archive");
-            request.setDestinationInExternalFilesDir(context, Environment.DIRECTORY_DOWNLOADS, "KeJu");
+            request.setDestinationInExternalFilesDir(context, Environment.DIRECTORY_DOWNLOADS, title);
+            request.setVisibleInDownloadsUi(true);
             downloadId = dm.enqueue(request);
         }
         return downloadId;
+    }
+
+
+    /**
+     * TODO 开始下载
+     *
+     * @param url      下载地址
+     * @param path     存储地址
+     * @param fileName 文件名
+     */
+    public void download(String url, String path, String fileName) {
+        //需要处理
+    }
+
+    public class Build {
+
+        private String url;
+        private String path;
+        private String fileName;
+        private DownloadListener listener;
+
+        public String getUrl() {
+            return url;
+        }
+
+        public Build setUrl(String url) {
+            this.url = url;
+            return this;
+        }
+
+        public String getPath() {
+            return path;
+        }
+
+        public Build setPath(String path) {
+            this.path = path;
+            return this;
+        }
+
+        public String getFileName() {
+            return fileName;
+        }
+
+        public Build setFileName(String fileName) {
+            this.fileName = fileName;
+            return this;
+        }
+
+        public void build(DownloadListener listener) {
+            this.listener = listener;
+            //开始下载任务
+            download(url, path, fileName);
+        }
+    }
+
+    interface DownloadListener {
+
+        void onStart();
+
+        void onProgress(int progress);
+
+        void onDone();
+
+        void onFailure(String msg);
     }
 }
